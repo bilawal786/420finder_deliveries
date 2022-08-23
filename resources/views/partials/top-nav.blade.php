572d5754d5bec261b9414dd98890ab139ba68373
Zohaib Hasan <104619325+zohaib173@users.noreply.github.com>
2022-08-23 17:24:30 +0500

    dec

--- resources/views/partials/top-nav.blade.php
+++ resources/views/partials/top-nav.blade.php
@@ -43,17 +43,36 @@
             $check = DB::table('businesses')->where('id', '=', session('business_id'))->where('latitude','=',null)->where('longitude','=',null)->first();
             ?>
             @if(($currentDate >= $startDate) && ($currentDate <= $endDate))
-                @if($check)
-                    <div class="navbar-btn">
-                        <p class="txt"><b>Please Update Your Profile. </b></p>
-                    </div>
-                @endif
+
+                <div class="navbar-btn">
+
+                    <p class="txt"><b>
+                            @if($check )
+                                Please Update Your Profile. <a href="{{route('accountsettings')}}">Click Here</a>
+                            @endif
+                            @if($details)
+                                Please Add Details. <a href="{{route('detail.index')}}">Click Here</a>
+                            @endif
+                        </b>
+                    </p>
+
+
+
+                </div>
+
             @else
                 <div class="navbar-btn">
-                    <p class="txt"><b>You dont have any subscription your product menu is no more available on website. <a href="{{route('subscription')}}">Please purchase a subscription</a></b></p>
-                    @if($check)
-                        <p class="txt"><b>Please Update Your Profile. </b></p>
-                    @endif
+                    <p class="txt"><b>
+                            You dont have any subscription your product menu is no more available on website. <a href="{{route('subscription')}}">Please purchase a subscription</a>
+                            @if($check)
+                                Please Update Your Profile. <a href="{{route('accountsettings')}}">Click Here</a>
+                            @endif
+                            @if($details)
+                                Please Add Details. <a href="{{route('detail.index')}}">Click Here</a>
+                            @endif
+                        </b>
+                    </p>
+
                 </div>
 
             @endif
